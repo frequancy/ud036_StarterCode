8b4fdeb722cf276b3c168fc9e1ca4581d2428d21
Mustafa El-Kikhia <bufaomar90@icloud.com>
2018-05-27 20:11:03 +0200

    added the "show_trailer" method to the media file

--- media.py
+++ media.py
@@ -1,9 +1,14 @@
 """this modul is for constructing a class that defines the movies we're giving
 to the web page """
 
+import webbrowser
+
 class Movie():
     def __init__(self, movie_title, movie_story, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_story
         self.poster_img_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
