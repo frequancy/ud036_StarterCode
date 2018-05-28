ef2ffcfffb1ac9e9bc624b836772f5d3f6397936
Mustafa El-Kikhia <bufaomar90@icloud.com>
2018-05-28 22:41:09 +0200

    some modification

--- media.py
+++ media.py
@@ -4,11 +4,10 @@ to the web page """
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, movie_story, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_story
-        self.poster_img_url = poster_image
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.poster_img_url)
